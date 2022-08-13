164ccff7758c244ea2d885815f7a98ba66fcbb02
Guilherme <guilherme.llopreti@gmail.com>
2022-08-13 17:09:20 -0300

    feat: deploy configs

--- kenzie_pet/settings.py
+++ kenzie_pet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 import dotenv
 import environ
 
@@ -95,6 +96,13 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+    )
+    DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
